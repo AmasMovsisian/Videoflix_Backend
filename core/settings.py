9eef4c23d4defaf573e42904f0c349c063b2b65e
AmasMovsisian <232450988+AmasMovsisian@users.noreply.github.com>
2026-08-07 01:37:29 +0200

    Configure SMTP email delivery

--- core/settings.py
+++ core/settings.py
@@ -197,7 +197,7 @@ CORS_ALLOWED_ORIGINS = CSRF_TRUSTED_ORIGINS
 CORS_ALLOW_CREDENTIALS = True
 
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 EMAIL_HOST = os.getenv("EMAIL_HOST")
 
