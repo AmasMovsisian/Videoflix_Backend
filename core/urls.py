59ff4f762848f9800208e9d4c9fbd88ee3530085
AmasMovsisian <232450988+AmasMovsisian@users.noreply.github.com>
2026-08-07 05:00:04 +0200

    fix(media): serve uploaded thumbnails through django media urls

--- core/urls.py
+++ core/urls.py
@@ -17,9 +17,35 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("api/",include("accounts.api.urls"),),
-    path("api/",include("videos.api.urls"),),
-    path("django-rq/",include("django_rq.urls")),
+    path(
+        "admin/",
+        admin.site.urls
+    ),
+
+    path(
+        "api/",
+        include("accounts.api.urls")
+    ),
+
+    path(
+        "api/",
+        include("videos.api.urls")
+    ),
+
+    path(
+        "django-rq/",
+        include("django_rq.urls")
+    ),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
